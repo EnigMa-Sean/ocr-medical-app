e11cdbc8452fcb95046eb62333a905c95ac740b6
oak.psc <pisitchai.tac@student.mahidol.edu>
2023-12-11 00:29:46 +0700

    Finished UI (create template)

--- ui_tinkercad/ui_tinkercad.py
+++ ui_tinkercad/ui_tinkercad.py
@@ -14,29 +14,33 @@ def initiate_ocr():
         try:
             # Perform OCR process here
             # Add Yun OCR Code here
-            print(f"OCR initiated for Folder: {folder_path}, Excel File: {excel_name}")
-
-            # Simulate OCR completion message
+            #print(f"OCR initiated for Folder: {folder_path}, Excel File: {excel_name}")
             success_message = f"OCR is finished and saved at {excel_name}"  # Replace output_path with the actual path
             show_success(success_message)
 
         except Exception as e:
             show_error(f"Error during OCR process: {str(e)}")
 
+
+
 def create_template_window():
     template_window = tk.Toplevel(window)
     template_window.title("Create Template")
-    template_window.geometry("300x300")
+    template_window.geometry("500x500")
 
     # Buttons in a grid layout
-    header_button = tk.Button(template_window, text="Header", command=lambda: label_function_example("Header"))
-    header_button.grid(row=0, column=0, padx=10, pady=10)
+    loadtemplate_button = tk.Button(template_window, text="Load Template", command=lambda: label_function_example("Select Template"), width=15)
+    loadtemplate_button.grid(row=0, column=2, padx=10, pady=10)
 
-    value_button = tk.Button(template_window, text="Value", command=lambda: label_function_example("Value"))
-    value_button.grid(row=0, column=1, padx=10, pady=10)
+    header_button = tk.Button(template_window, text="Header", command=lambda: label_function_example("Header"), width=15)
+    header_button.grid(row=1, column=1, padx=10, pady=10)
 
-    save_button = tk.Button(template_window, text="Save to Template", command=lambda: label_function_example("Save"))
-    save_button.grid(row=0, column=2, padx=10, pady=10)
+    value_button = tk.Button(template_window, text="Value", command=lambda: label_function_example("Value"), width=15)
+    value_button.grid(row=1, column=2, padx=10, pady=10)
+
+    save_button = tk.Button(template_window, text="Save to Template", command=lambda: label_function_example("Save"), width=15)
+    save_button.grid(row=1, column=3, padx=10, pady=10)
+
 
 def label_function_example(label_type):
      print(f"Label Function Called for {label_type}")
@@ -58,43 +62,33 @@ def browse_excel_path():
     excel_name_entry.insert(0, excel_path)
 
 def select_file_type():
-    file_type = folder_path_entry.get()
-
-    if file_type == "Image":
-        selected_file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
-        display_file(selected_file_path)
-        return "image"
-
-    elif file_type == "PDF":
-        selected_file_path = filedialog.askopenfilename(filetypes=[("PDF Files", "*.pdf")])
-        pdf_images = convert_pdf_to_images(selected_file_path)
-        if pdf_images:
-            display_file(pdf_images[0])
-        return "pdf"
-    else:
-        selected_file_path = ""
+    return file_type_var.get()
 
 def display_file(file_path):
     try:
-        # Check if the file is a PDF
-        if file_path.lower().endswith('.pdf'):
+        file_type = select_file_type()
+
+        if file_type == "Image":
+            # For image files
+            image = Image.open(file_path)
+        elif file_type == "PDF":
+            # For PDF files
             pdf_images = convert_pdf_to_images(file_path)
             if pdf_images:
-                # Display the first page of the PDF
                 image = pdf_images[0]
             else:
                 raise Exception("Error converting PDF to images.")
         else:
-            # For other image files
-            image = Image.open(file_path)
+            raise Exception("Unsupported file type.")
 
         # Display the image
-        image = image.resize((300, 300), Image.BILINEAR)
+        image = image.resize((500, 705), Image.BILINEAR)
         img = ImageTk.PhotoImage(image)
-        display_canvas.config(width=300, height=300)
+        display_canvas.config(width=500, height= 705)
         display_canvas.create_image(0, 0, anchor=tk.NW, image=img)
         display_canvas.image = img
         error_label.config(text="")
+        show_success("File displayed successfully.")
     except Exception as e:
         show_error(f"Error displaying file: {str(e)}")
 
@@ -157,6 +151,8 @@ main_frame.pack(fill=tk.BOTH, expand=True)
 left_frame = ttk.Frame(main_frame, padding="10", style="left.TFrame")
 left_frame.pack(side=tk.LEFT, fill=tk.Y, expand=False)
 
+left_frame.grid_propagate(True)  # Prevent the frame from resizing
+
 # Apply a separator between frames
 ttk.Separator(main_frame, orient=tk.VERTICAL).pack(side=tk.LEFT, fill=tk.Y)
 
@@ -172,10 +168,18 @@ style.configure("left.TFrame", background=background_color)
 title_font = ("Helvetica", 20, "bold")
 button_font = ("Helvetica", 12)
 
+#Right Frame configuration
+datapreview_label = tk.Label(right_frame, text="Data Preview", font=button_font, bg=background_color, fg=text_color)
+datapreview_label.pack(pady=10)
+
+display_canvas = tk.Canvas(right_frame, width=500, height=705, bg=background_color)
+display_canvas.pack(pady=10)
+
+#Left Frame configuration
 title_label = tk.Label(left_frame, text="OCR Application", font=title_font, bg=background_color, fg=text_color)
 title_label.pack(pady=10)
 
-folder_path_label = tk.Label(left_frame, text="Folder Path:", font=button_font, bg=background_color, fg=text_color)
+folder_path_label = tk.Label(left_frame, text="1. Select Your file for OCR:", font=button_font, bg=background_color, fg=text_color)
 folder_path_label.pack()
 
 folder_path_entry = tk.Entry(left_frame, width=40, font=("Helvetica", 10))
@@ -184,10 +188,10 @@ folder_path_entry.pack(pady=5)
 browse_button = tk.Button(left_frame, text="Browse", command=browse_folder_path, bg=accent_color, fg="white", font=button_font, width=20)
 browse_button.pack(pady=5)
 
-folder_label = tk.Label(left_frame, text="Selected Folder:", font=("Helvetica", 10), bg=background_color, fg=text_color)
+folder_label = tk.Label(left_frame, text="Selected File:", font=("Helvetica", 10), bg=background_color, fg=text_color)
 folder_label.pack(pady=5)
 
-excel_name_label = tk.Label(left_frame, text="Excel File Path:", font=button_font, bg=background_color, fg=text_color)
+excel_name_label = tk.Label(left_frame, text="2. Select Excel file for saving result:", font=button_font, bg=background_color, fg=text_color)
 excel_name_label.pack()
 
 excel_name_entry = tk.Entry(left_frame, width=40, font=("Helvetica", 10))
@@ -196,11 +200,8 @@ excel_name_entry.pack(pady=5)
 browse_excel_button = tk.Button(left_frame, text="Browse", command=browse_excel_path, bg=accent_color, fg="white", font=button_font, width=20)
 browse_excel_button.pack(pady=5)
 
-file_type_label = tk.Label(left_frame, text="Select File Type:", font=button_font, bg=background_color, fg=text_color)
+file_type_label = tk.Label(left_frame, text="3. Select File Type (Image or PDF):", font=button_font, bg=background_color, fg=text_color)
 file_type_label.pack()
-
-display_canvas = tk.Canvas(right_frame, width=300, height=300, bg=background_color)
-display_canvas.pack(pady=10)
 
 file_type_var = tk.StringVar()
 file_type_var.set("Image")
@@ -209,6 +210,15 @@ file_type_menu = tk.OptionMenu(left_frame, file_type_var, "Image", "PDF")
 file_type_menu.config(bg=accent_color, fg="white", font=button_font)
 file_type_menu.pack(pady=5)
 
+# Create a button for the "Display Selected File" function
+display_file_button = tk.Button(left_frame, text="Display Selected File", command=display_selected_file, bg=accent_color, fg="white", font=button_font, width=20)
+display_file_button.pack(pady=10)
+display_file_button.config(command=display_selected_file)
+
+# Create a button for the "Create Template" function
+create_template_button = tk.Button(left_frame, text="Template", command=create_template_window, bg=accent_color, fg="white", font=button_font, width=20)
+create_template_button.pack(pady=5)
+
 # Create a button for the label function
 label_button = tk.Button(left_frame, text="Label", command=label_function, bg=accent_color, fg="white", font=button_font, width=20)
 label_button.pack(pady=5)
@@ -216,15 +226,6 @@ label_button.pack(pady=5)
 # Create a button for the ocr function
 ocr_button = tk.Button(left_frame, text="Initiate OCR", command=initiate_ocr, bg=accent_color, fg="white", font=button_font, width=20)
 ocr_button.pack(pady=5)
-
-# Create a button for the "Create Template" function
-create_template_button = tk.Button(left_frame, text="Create Template", command=create_template_window, bg=accent_color, fg="white", font=button_font, width=20)
-create_template_button.pack(pady=5)
-
-# Create a button for the "Display Selected File" function
-display_file_button = tk.Button(left_frame, text="Display Selected File", command=display_selected_file, bg=accent_color, fg="white", font=button_font, width=20)
-display_file_button.pack(pady=10)
-display_file_button.config(command=display_selected_file)
 
 # Bind the select_file_type function to the Configure event of file_type_menu
 file_type_menu.bind("<Configure>", lambda event: select_file_type())
